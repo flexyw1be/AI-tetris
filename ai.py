d6f0900a30f900b301394ed19d0a60e78a765ae6
flexyw1be <geba_m@mail.ru>
2024-04-24 23:09:37 +0700

    + new AI (check game.py and ai.py) + new get_score()(check ai.py) + code refactoring + new TODO(check main.py) +

--- ai.py
+++ ai.py
@@ -1,26 +1,26 @@
 # система «штрафов» для расчета оптимального хода
-height = -0.5  # высота
+height = -200  # высота
 clears = 8  # очистка линии
 holes = -7.5  # дырка
 blockades = -3.5  # блокада
-block = 50  # касание блока
+block = 200  # касание блока
 wall = 0.05  # касание стены
-floor = 50  # касание пола
+floor = 200
 
 
 def get_score(lst, cords):
     print("чистые линии: ", get_clear_lines(lst))
-    print("высота: ", get_height(lst))
+    print("высота: ", get_height(lst, cords))
     print("дырки: ", get_holes(lst))
     print("блоки: ", get_taken_blocks(lst, cords))
-    print("стены: ", get_taken_walls(lst))
+    print("стены: ", get_taken_walls(lst, cords))
     print("пол: ", get_taken_floor(lst))
 
-    summ = height * get_height(lst) + \
+    summ = height * get_height(lst, cords) + \
            clears * get_clear_lines(lst) + \
            holes * get_holes(lst) + \
            block * get_taken_blocks(lst, cords) + \
-           wall * get_taken_walls(lst) + \
+           wall * get_taken_walls(lst, cords) + \
            floor * get_taken_floor(lst)
     return summ
 
@@ -40,9 +40,9 @@ def get_taken_blocks(lst, cords):
     return s
 
 
-def get_taken_walls(lst):
+def get_taken_walls(lst, cords):
     summ = 0
-    for i in lst:
+    for i in cords:
         if i % 10 == 0:
             summ += 1
         if i % 10 == 9:
@@ -66,5 +66,5 @@ def get_clear_lines(lst):
     return 0
 
 
-def get_height(lst):
-    return 20 - min(lst) // 10
+def get_height(lst, cords):
+    return (20 -min(cords) // 10) * len(cords)
